e823080eb38bbbe7257962d9ff1fe180c3ed54e2
MoNouri97 <MoNouri97@users.noreply.github.com>
2022-06-18 23:43:40 +0000

    deploy: 969b1feb1b00827930372a4170ce9ecd5e90219b

--- _next/static/JyZu-vVzqdGJ6qIu3lsDN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/490-5084ce5dc95c7777.js","static/chunks/pages/index-18130414cc2c32de.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
